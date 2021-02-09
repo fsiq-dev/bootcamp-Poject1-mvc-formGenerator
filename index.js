7525b2e5163bbd1765c15ee3f1b165519363a7f8
Felipe Siqueira <felipe.msiqueira@al.infnet.edu.br>
2021-02-09 17:03:32 -0300

    create index view

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 import express from 'express'
 const app = express();
+import path from 'path'
+const __dirname = path.resolve();
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname + '/public')));
+
 
 import routes from './routes/routes.js'
 app.use ('/', routes);
